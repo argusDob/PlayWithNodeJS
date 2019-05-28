972532aa9c5ee4d3f650709c37d4dbf41fb1861e
Ioannis Dimitriou <44471356+argusDob@users.noreply.github.com>
2019-05-28 15:35:54 +0200

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -15,6 +15,8 @@ app.get('/', function(req, res, next) {
             })
         } else {
             // render to views/user/list.ejs template file
+            console.log(req.body);
+            console.log("I am a response, " ,res);
             res.render('user/list', {
                 title: 'User List', 
                 data: result
@@ -34,6 +36,81 @@ app.get('/add', function(req, res, next){
         email: ''        
     })
 })
+
+app.post('/add', function(req, res, next){    
+    req.assert('name', 'Name is required').notEmpty()           //Validate name
+    req.assert('age', 'Age is required').notEmpty()             //Validate age
+    req.assert('email', 'A valid email is required').isEmail()  //Validate email
+ 
+    var errors = req.validationErrors()
+    
+    if( !errors ) {   //No errors
+    
+        var user = {
+
+            //Escaping is the process of securing output by stripping out unwanted data, 
+            //like malformed HTML or script tags, preventing this data from being seen as code.
+            name: req.sanitize('name').escape(),     
+            age: req.sanitize('age').escape(),
+            email: req.sanitize('email').escape()
+        }
+                 
+        req.db.collection('users').insert(user, function(err, result) {
+            if (err) {
+                req.flash('error', err)
+                
+                // render to views/user/add.ejs
+                res.render('user/add', {
+                    title: 'Add New User',
+                    name: user.name,
+                    age: user.age,
+                    email: user.email                    
+                })
+            } else {                
+                req.flash('success', 'Data added successfully!')
+                
+                // redirect to user list page                
+                res.redirect('/users')
+                
+            }
+        })        
+    }
+    else {   //Display errors to user
+        var error_msg = ''
+        errors.forEach(function(error) {
+            error_msg += error.msg + '<br>'
+        })                
+        req.flash('error', error_msg)        
+        
+        /**
+         * Using req.body.name 
+         * because req.param('name') is deprecated
+         */ 
+        res.render('user/add', { 
+            title: 'Add New User',
+            name: req.body.name,
+            age: req.body.age,
+            email: req.body.email
+        })
+    }
+})
+
+// DELETE USER
+app.delete('/delete/(:id)', function(req, res, next) {    
+    var o_id = new ObjectId(req.params.id)
+    req.db.collection('users').remove({"_id": o_id}, function(err, result) {
+        if (err) {
+            req.flash('error', err)
+            // redirect to users list page
+            res.redirect('/users')
+        } else {
+            req.flash('success', 'User deleted successfully! id = ' + req.params.id)
+            // redirect to users list page
+            res.redirect('/users')
+        }
+    })    
+})
+ 
 
 
 module.exports = app
